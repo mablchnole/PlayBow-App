5c322d295f1785be8bc7b3afaf6094983a7e605e
mablchnole <mablchnole@gmail.com>
2016-07-24 16:21:33 -0500

    delete fave working

--- server/app.js
+++ server/app.js
@@ -27,9 +27,13 @@ app.get('/', function(req, res){
   res.sendFile(path.resolve('client/views/index.html'));
 });
 
-// set up server
-app.set('port', process.env.PORT || 1234);
-
-app.listen(app.get('port'), function() {
-  console.log('human, are you awake:', app.get('port'));
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
+
+// set up server
+// app.set('port', process.env.PORT || 1234);
+//
+// app.listen(app.get('port'), function() {
+//   console.log('human, are you awake:', app.get('port'));
+// });
